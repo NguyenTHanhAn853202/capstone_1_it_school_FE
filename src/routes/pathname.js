6044c452159c3e0924af134ad279347ad9fd6615
kimquynh <kimquynh270703@gmail.com>
2024-10-09 17:22:06 +0700

    verifycodesuccesfully

--- src/routes/pathname.js
+++ src/routes/pathname.js
@@ -6,12 +6,14 @@ import Lading from '~/pages/Lading';
 import LoginRegister from '~/pages/LoginRegister';
 const ForgotPassword = React.lazy(() => import('~/components/ForgotPassword'));
 const VerifyCode = React.lazy(() => import('~/components/ForgotPasswordOtp'));
+const VerifyCodeSeccesfully = React.lazy(() => import('~/components/VerifyCodeSuccesfully'));
 
 export const publicPath = [
     { pathname: '/', Element: Home },
     { pathname: '/experience', Element: Lading, noLayout: true },
     { pathname: '/forgot-password', Element: ForgotPassword },
-    {pathname:"/verify-code", Element: VerifyCode}
+    {pathname:"/verify-code", Element: VerifyCode},
+    {pathname:"/verify-code-succesfullt", Element: VerifyCodeSeccesfully}
 ];
 
 export const privatePath = [];
